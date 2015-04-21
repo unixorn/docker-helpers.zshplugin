e79da6880f695d42df0a9db38a8cde148ba022b0
Joe Block <jpb@unixorn.net>
2015-04-20 23:17:48 -0700

    Add check that docker is in $PATH before making aliases using it

--- docker-helpers.plugin.zsh
+++ docker-helpers.plugin.zsh
@@ -12,65 +12,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Add our plugin's bin diretory to user's path
-PLUGIN_D="$(dirname $0)"
-export PATH=${PATH}:${PLUGIN_D}/bin
+if (which docker > /dev/null)
+then
+  # Add our plugin's bin diretory to user's path
+  PLUGIN_D="$(dirname $0)"
+  export PATH=${PATH}:${PLUGIN_D}/bin
 
-# Most of these aliases are from http://www.kartar.net/2014/03/some-useful-docker-bash-functions-and-aliases/
-alias dkd="docker run -d -P"
-alias dki="docker run -t -i -P"
-alias dkl='docker ps -l -q'
-alias docker-daemonize="docker run -d -P"
-alias docker-interactive="docker run -t -i -P"
-alias docker-last='docker ps -l -q'
-alias dockerbuild='docker build'
-alias dockerimages='docker images'
-alias dockerps='docker ps'
+  # Most of these aliases are from http://www.kartar.net/2014/03/some-useful-docker-bash-functions-and-aliases/
+  alias dkd="docker run -d -P"
+  alias dki="docker run -t -i -P"
+  alias dkl='docker ps -l -q'
+  alias docker-daemonize="docker run -d -P"
+  alias docker-interactive="docker run -t -i -P"
+  alias docker-last='docker ps -l -q'
+  alias dockerbuild='docker build'
+  alias dockerimages='docker images'
+  alias dockerps='docker ps'
 
-dkb() {
-  docker build -t="$1" .
-}
+  dkb() {
+    docker build -t="$1" .
+  }
 
-docker-ip() {
-  docker inspect --format "{{ .NetworkSettings.IPAddress }}" $1
-}
+  docker-ip() {
+    docker inspect --format "{{ .NetworkSettings.IPAddress }}" $1
+  }
 
-docker-pid() {
-  docker inspect --format "{{ .State.Pid }}" $1
-}
+  docker-pid() {
+    docker inspect --format "{{ .State.Pid }}" $1
+  }
 
-docker-zsh() {
-  local TAG=$1
-  docker run -v /tmp:/host_tmp:rw -i -t $TAG /bin/zsh
-}
+  docker-zsh() {
+    local TAG=$1
+    docker run -v /tmp:/host_tmp:rw -i -t $TAG /bin/zsh
+  }
 
-dps-monitor() {
-   while true
-   do
-       clear
-       docker ps | cut -c -$(tput cols)
-       sleep 0.5
-   done
-}
+  dps-monitor() {
+     while true
+     do
+         clear
+         docker ps | cut -c -$(tput cols)
+         sleep 0.5
+     done
+  }
 
-# helpers for starting a container with access to the current directory
+  # helpers for starting a container with access to the current directory
 
-docker-busybox() {
-  docker run -v $(pwd):/shared --rm -it busybox:latest /bin/sh
-}
+  docker-busybox() {
+    docker run -v $(pwd):/shared --rm -it busybox:latest /bin/sh
+  }
 
-docker-centos() {
-  docker run -v $(pwd):/shared --rm -it centos:centos7 /bin/bash
-}
+  docker-centos() {
+    docker run -v $(pwd):/shared --rm -it centos:centos7 /bin/bash
+  }
 
-docker-centos6() {
-  docker run -v $(pwd):/shared --rm -it centos:centos6 /bin/bash
-}
+  docker-centos6() {
+    docker run -v $(pwd):/shared --rm -it centos:centos6 /bin/bash
+  }
 
-docker-fedora() {
-  docker run -v $(pwd):/shared --rm -it fedora:20 /bin/bash
-}
+  docker-fedora() {
+    docker run -v $(pwd):/shared --rm -it fedora:20 /bin/bash
+  }
 
-docker-ubuntu() {
-  docker run -v $(pwd):/shared --rm -it ubuntu:14.04 /bin/bash
-}
+  docker-ubuntu() {
+    docker run -v $(pwd):/shared --rm -it ubuntu:14.04 /bin/bash
+  }
+fi
+
