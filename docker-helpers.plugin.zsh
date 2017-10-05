33d985f8da06078de80622f8172950bbd5c9e9c4
Joe Block <jpb@unixorn.net>
2017-10-05 10:43:36 -0600

    Make the docker checks stop spamming the terminal

--- docker-helpers.plugin.zsh
+++ docker-helpers.plugin.zsh
@@ -1,18 +1,18 @@
-# Copyright 2014 Joseph Block <jpb@unixorn.net>
+# Copyright 2014-2017 Joseph Block <jpb@unixorn.net>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if (which docker > /dev/null)
+if (which docker &> /dev/null)
 then
   # Add our plugin's bin diretory to user's path
   PLUGIN_D="$(dirname $0)"
@@ -105,7 +105,7 @@ then
   }
 fi
 
-if (which docker-compose > /dev/null)
+if (which docker-compose &> /dev/null)
 then
   alias d-cp=docker-compose
 fi
